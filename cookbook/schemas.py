ad295dfc5303b00777bb3eeaf5681c1e74504e1b
Sumeet Shinde <sumeetshinde118@gmail.com>
2024-12-04 15:39:44 +0530

    Schema modifications

--- cookbook/schemas.py
+++ cookbook/schemas.py
@@ -1,35 +1,35 @@
-import graphene 
+import graphene
 from graphene_django import DjangoObjectType
 from ingredients.models import Category, Ingredient
 
-
-class CategoryType(DjangoObjectType):
-    class Meta:
-        Model = Category
-        fields = ('id', 'name', 'ingredients')
-
-
 class IngredientType(DjangoObjectType):
     class Meta:
-        Model = Ingredient
-        fields = ('id', 'name', 'notes', 'category')
+        model = Ingredient
+        fields = ("id", "name", "notes", "category")
+
+
+class CategoryType(DjangoObjectType):
+    ingredients = graphene.List(IngredientType)
+
+    class Meta:
+        model = Category
+        fields = ("id", "name", "ingredients")
+
+    def resolve_ingredients(self, info):
+        return self.ingredient_set.all()
 
 
 class Query(graphene.ObjectType):
     all_ingredients = graphene.List(IngredientType)
-    category_by_name = graphene.List(CategoryType, name=graphene.String(required=True))
+    category_by_name = graphene.Field(CategoryType, name=graphene.String(required=True))
 
     def resolve_all_ingredients(root, info):
         return Ingredient.objects.select_related("category").all()
 
-    def resolve_categorooy_by_name(root, info, name):
+    def resolve_category_by_name(root, info, name):
         try:
-            name=Category.objects.get(name=name)
-        except Category.DoesNotExists:
-            return None 
+            return Category.objects.get(name=name)
+        except Category.DoesNotExist:
+            return None
 
-schema = graphene.schema(query=Query)
-
-
-
-
+schema = graphene.Schema(query=Query)
--- ingredients/models.py
+++ ingredients/models.py
@@ -2,24 +2,24 @@ from django.db import models
 
 
 class Category(models.Model):
-    name=models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
 
     class Meta:
-        verbose_name='category'
-        verbose_name_plural='categories'
-
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+   
     def __str__(self):
         return self.name
 
 
 class Ingredient(models.Model):
-    name=models.CharField(max_length=50)
-    notes=models.TextField()
-    category=models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    notes = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     class Meta:
-        verbose_name='Ingredient'
-        verbose_name_plural='Ingredients'
+        verbose_name = 'Ingredient'
+        verbose_name_plural = 'Ingredients'
 
     def __str__(self):
         return self.name
